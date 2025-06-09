6748eb5f0306364e24a87e2108caa701dc1c7d4c
shrutidhungana <shrutidhungana123@gmail.com>
2025-06-09 21:57:44 +0545

    Completed delete to_do: 1)Completed delete and all

--- server/routes/todo-routes.ts
+++ server/routes/todo-routes.ts
@@ -1,10 +1,16 @@
 import express, { Router } from "express";
-import { addTodo, listTodos, updateTodo } from "../controllers/todo-controller";
+import {
+  addTodo,
+  listTodos,
+  updateTodo,
+  deleteTodo,
+} from "../controllers/todo-controller";
 
 const router: Router = express.Router();
 
 router.post("/add", addTodo as express.RequestHandler);
 router.get("/list", listTodos as express.RequestHandler);
 router.put("/update/:id", updateTodo as express.RequestHandler);
+router.delete("/delete/:id", deleteTodo as express.RequestHandler); 
 
 export default router;
