4a78b2daddc67597f388b0eb9b1f7ef3bfeb3c00
shrutidhungana <shrutidhungana123@gmail.com>
2025-06-09 17:46:03 +0545

    Modals created for To do: 1)Modals with schema created

---
+++ server/modals/Todo.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ITodo extends Document {
+  name: string;
+  shortDescription: string;
+  dateTime: Date;
+  done: boolean;
+}
+
+const TodoSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  shortDescription: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dateTime: {
+    type: Date,
+    required: true,
+  },
+  done: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model<ITodo>("Todo", TodoSchema);
